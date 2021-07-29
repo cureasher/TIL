25328d160abc1345927c8c1f39a6e42c6f274f9a
cureasher <cureasher@gmail.com>
2021-07-29 23:20:41 +0900

    feat: create 210729-1-005-ArithmeticOperator.kt

---
+++ kotlin/ex-200/src/210729-1-005-ArithmeticOperator.kt
@@ -0,0 +1,13 @@
+package code
+
+fun main(args: Array<String>) {
+    val number: Int = 15 - 4 * 3
+    val number2: Int = 65 % 7
+    val number3: Double = 7.5/5 + 22.25
+    val number4: Double = number/number2+ 0.7
+
+    println(number)
+    println(number2)
+    println(number3)
+    println(number4)
+}
