ba59ef26d9e96f219cd149859dd5ce38797088f5
cureasher <cureasher@gmail.com>
2021-07-29 22:29:14 +0900

    feat: create 210729-1-003-Variable.kt

---
+++ kotlin/ex-200/src/210729-1-003-Variable.kt
@@ -0,0 +1,17 @@
+package code
+
+fun main(args: Array<String>) : Unit {
+    var totalNumber : Int
+    totalNumber = 0
+
+    val firstNumber : Int = 10 + 53 - 7
+    println(firstNumber)
+
+    val secondNumber = 43 + 75 + firstNumber
+    println(secondNumber)
+
+    totalNumber =- firstNumber + secondNumber
+
+    println(totalNumber)
+
+}
