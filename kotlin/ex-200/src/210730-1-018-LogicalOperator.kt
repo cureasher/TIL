9cc717d4332aa3717e93adecbbc9281e17e9be51
cureasher <cureasher@gmail.com>
2021-07-30 22:52:10 +0900

    feat: create 210730-1-018-LogicalOperator.kt

---
+++ kotlin/ex-200/src/210730-1-018-LogicalOperator.kt
@@ -0,0 +1,13 @@
+package code
+
+fun main(args: Array<String>) : Unit {
+    val a = 15
+    val b = 17
+
+    var bool: Boolean = (a - b < a + b) && (a == 15)
+    println(bool)
+
+    bool = !((a+b) >(a *3)||(b-a) >0)
+    println(bool)
+
+}
