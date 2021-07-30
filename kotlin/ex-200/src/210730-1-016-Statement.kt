51b0cb6d3c8c0b85c14544ba308551e58e6c0551
cureasher <cureasher@gmail.com>
2021-07-30 22:24:57 +0900

    feat: create 210730-1-016-Statement.kt

---
+++ kotlin/ex-200/src/210730-1-016-Statement.kt
@@ -0,0 +1,13 @@
+package code
+
+fun main(args: Array<String>) : Unit {
+    val num: Int
+    num = 15
+
+    println(num +7 * 3)
+
+    // 문장 한줄로 연결
+    val num2: Int; num2 = 15; println(num2 +7 * 3)
+
+
+}
