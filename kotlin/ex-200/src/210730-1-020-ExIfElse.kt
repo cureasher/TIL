d75f15dc0941b2e7d5f347e5dd31003080ee37a7
cureasher <cureasher@gmail.com>
2021-07-30 23:03:33 +0900

    feat: create 210730-1-020-ExIfElse.kt

---
+++ kotlin/ex-200/src/210730-1-020-ExIfElse.kt
@@ -0,0 +1,16 @@
+package code
+
+fun main(args: Array<String>) : Unit {
+    val a = 10
+    val b = 5
+    if (a < b)
+        println("if")
+    else
+        println("false : else")
+
+    if (a > b)
+        println("true : a가 크다")
+    else
+        println("b가 a보다 크다")
+
+}
