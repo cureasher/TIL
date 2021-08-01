64bba8be0be2e8f8da07cca8628729a5d5f0a9dc
cureasher <cureasher@gmail.com>
2021-08-01 16:21:57 +0900

    feat: create 210801-1-034-VariableArgumentSum.kt

---
+++ kotlin/ex-200/src/210801-1-034-VariableArgumentSum.kt
@@ -0,0 +1,18 @@
+package code
+
+fun main(args: Array<String>) {
+    println(getSumOf(1,2,3,4,5,6,7))
+    println(getSumOf(32,57,12))
+    println(getSumOf())
+}
+
+fun getSumOf(vararg numbers: Int) : Int{
+    val count = numbers.size
+    var index = 0; var sum = 0
+
+    while (index < count){
+        sum += numbers[index]
+        index++
+    }
+    return sum
+}
