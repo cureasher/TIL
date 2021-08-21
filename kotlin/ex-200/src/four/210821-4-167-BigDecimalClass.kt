375244fa47e1918efc6ab7fd4059dd9fba44847a
cureasher <cureasher@gmail.com>
2021-08-21 21:16:19 +0900

    feat: create 210821-4-167-BigDecimalClass.kt

---
+++ kotlin/ex-200/src/four/210821-4-167-BigDecimalClass.kt
@@ -0,0 +1,8 @@
+package code.four
+
+fun main(args: Array<String>) {
+    val firstNumber = 3.0000000000003.toBigDecimal()
+    val secondNumber = 4.0000000000004.toBigDecimal()
+
+    println(firstNumber * secondNumber)
+}
