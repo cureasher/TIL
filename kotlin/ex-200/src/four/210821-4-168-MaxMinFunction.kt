cc36fbded4c61225d6766c5287330b668eb747b0
cureasher <cureasher@gmail.com>
2021-08-21 21:18:18 +0900

    feat: create 210821-4-168-MaxMinFunction.kt

---
+++ kotlin/ex-200/src/four/210821-4-168-MaxMinFunction.kt
@@ -0,0 +1,11 @@
+package code.four
+
+import kotlin.math.max
+import kotlin.math.min
+
+fun main(args: Array<String>) {
+    println("3,7 중 최대값: ${max(3,7)}")
+    println("10.53, 1.15 중 최대값: ${max(10.53, 1.15)}")
+    println("18, 13 중 최소값: ${min(18, 13)}")
+    println("7.6, 20.01 중 최소값: ${min(7.6, 20.01)}")
+}
