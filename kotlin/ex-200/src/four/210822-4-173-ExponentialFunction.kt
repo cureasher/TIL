f0ccecf5daa54bcfa3c4d1087e51a65683576c46
cureasher <cureasher@gmail.com>
2021-08-22 20:44:27 +0900

    feat: create 210822-4-173-ExponentialFunction.kt

---
+++ kotlin/ex-200/src/four/210822-4-173-ExponentialFunction.kt
@@ -0,0 +1,10 @@
+package code.four
+
+import kotlin.math.*
+
+fun main(args: Array<String>) {
+    println("루트 2.0: ${sqrt(2.0)}")
+    println("2.0^0.5: ${2.0.pow(0.5)}")
+    println("e^2.0: ${exp(2.0)}")
+    println("e^2.0 -1: ${expm1(2.0)}")
+}
