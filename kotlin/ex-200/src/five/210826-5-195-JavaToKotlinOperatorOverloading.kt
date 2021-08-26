3fc6af22b1bfe4fa0400caa40031cbba5c9b7204
cureasher <cureasher@gmail.com>
2021-08-26 23:25:21 +0900

    feat: create 210826-5-195-JavaToKotlinOperatorOverloading.kt

---
+++ kotlin/ex-200/src/five/210826-5-195-JavaToKotlinOperatorOverloading.kt
@@ -0,0 +1,12 @@
+package code.five
+
+import five.JavaClass
+
+fun main(args: Array<String>) {
+    val java = JavaClass()
+    println(100 in java)
+    println(java[13])
+    println(java[28])
+    println(java[18])
+
+}
